09d652bea4ac819f12fb70d8551d721d86d32f17
hatdog42 <kristian.lillesand@gmail.com>
2023-12-13 15:05:43 +0100

    notebook

---
+++ Assets/scripts/SwichPage.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Switch : MonoBehaviour
+{
+    private bool _hasHapend;
+    
+    private Input_Controler _input;
+   
+    public GameObject[] background;
+    public int index;
+
+    void Start()
+    {
+        index = 0;
+        _input = GetComponent<Input_Controler>();
+    }
+        
+
+    void Update()
+    {
+        if(index >= 5)
+            index = 5 ; 
+
+        if(index < 0)
+            index = 0 ;
+        
+
+
+        if(index == 0)
+        {
+            background[0].gameObject.SetActive(true);
+        }
+
+        if (!_hasHapend)
+        {
+            _hasHapend = true;
+            switch (_input.moveDirection.x)
+            {
+                case > 0:
+                    Next();
+                    break;
+                case < 0: 
+                    Previous(); 
+                    break;
+            }
+        }
+        else if (_input.moveDirection.x == 0)
+        {
+            _hasHapend = false;
+        }
+
+        if (_input.openNoteBook) SceneManager.UnloadSceneAsync("Notbok");
+    }
+
+    private void Next()
+    {
+        index += 1;
+    
+        foreach (var t in background)
+        {
+            t.gameObject.SetActive(false);
+            background[index].gameObject.SetActive(true);
+        }
+        Debug.Log(index);
+    }
+
+    private void Previous()
+    {
+        index -= 1;
+    
+        for(int i = 0 ; i < background.Length; i++)
+        {
+            background[i].gameObject.SetActive(false);
+            background[index].gameObject.SetActive(true);
+        }
+        Debug.Log(index);
+    }
+
+    
+}
